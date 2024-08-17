361894244f0228845748a07fcdadc26f101731a6
vinay-prog-ind <vinayshinde897@gmail.com>
2024-08-17 23:57:31 +0530

    Initial draft

---
+++ src/util/toggleNav.js
@@ -0,0 +1,11 @@
+let menuList = document.getElementById("menuList");
+menuList.style.maxHeight = "0px";
+
+async function toggleMenu() {
+	if (menuList.style.maxHeight == "0px") {
+		menuList.style.maxHeight = "300px";
+	} else {
+		menuList.style.maxHeight = "0px";
+	}
+	console.log("clicked");
+}
